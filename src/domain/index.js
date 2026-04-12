db663abf50c8bc4c5ccbd18fe0a3ee21b1bb7b6c
PDybala123 <18526620672@163.com>
2026-04-12 16:50:38 +0800

    Update index.js

--- src/domain/index.js
+++ src/domain/index.js
@@ -1,15 +1,14 @@
 class Sudoku {
   constructor(input) {
-    this.grid = input
-      ? JSON.parse(JSON.stringify(input))
-      : Array(9).fill().map(() => Array(9).fill(0));
+    this.grid = input ? JSON.parse(JSON.stringify(input)) : Array(9).fill().map(() => Array(9).fill(0));
   }
 
   getGrid() {
     return JSON.parse(JSON.stringify(this.grid));
   }
 
-  guess({ row, col, value }) {
+  guess(move) {
+    const { row, col, value } = move;
     this.grid[row][col] = value;
   }
 
@@ -22,57 +21,57 @@ class Sudoku {
   }
 
   toString() {
-    return this.grid.map(r => r.join(' ')).join('\n');
+    return this.grid.map(row => row.join(' ')).join('\n');
   }
 }
 
 class Game {
-  constructor(sudoku) {
-    this.current = sudoku.clone();
-    this.history = [this.current.clone()];
-    this.currentIndex = 0;
+  constructor({ sudoku }) {
+    this.sudoku = sudoku.clone();
+    this.history = [sudoku.clone()];
+    this.currentStep = 0;
+  }
+
+  getSudoku() {
+    return this.sudoku.clone();
   }
 
   guess(move) {
-    const next = this.current.clone();
-    next.guess(move);
-
-    this.history = this.history.slice(0, this.currentIndex + 1);
-    this.history.push(next);
-    this.currentIndex++;
-    this.current = next;
+    const newSudoku = this.sudoku.clone();
+    newSudoku.guess(move);
+    this.history = this.history.slice(0, this.currentStep + 1);
+    this.history.push(newSudoku);
+    this.currentStep = this.history.length - 1;
+    this.sudoku = newSudoku;
   }
 
   undo() {
     if (this.canUndo()) {
-      this.currentIndex--;
-      this.current = this.history[this.currentIndex].clone();
+      this.currentStep--;
+      this.sudoku = this.history[this.currentStep].clone();
     }
   }
 
   redo() {
     if (this.canRedo()) {
-      this.currentIndex++;
-      this.current = this.history[this.currentIndex].clone();
+      this.currentStep++;
+      this.sudoku = this.history[this.currentStep].clone();
     }
   }
 
   canUndo() {
-    return this.currentIndex > 0;
+    return this.currentStep > 0;
   }
 
   canRedo() {
-    return this.currentIndex < this.history.length - 1;
-  }
-
-  getSudoku() {
-    return this.current.clone();
+    return this.currentStep < this.history.length - 1;
   }
 
   toJSON() {
     return {
-      history: this.history.map(s => s.toJSON()),
-      currentIndex: this.currentIndex
+      sudoku: this.sudoku.toJSON(),
+      history: this.history.map(h => h.toJSON()),
+      currentStep: this.currentStep
     };
   }
 }
@@ -82,18 +81,26 @@ export function createSudoku(input) {
 }
 
 export function createSudokuFromJSON(json) {
-  return new Sudoku(json.grid);
+  return Sudoku.fromJSON(json);
 }
 
-export function createGame(sudoku) {
-  return new Game(sudoku);
+export function createGame({ sudoku }) {
+  return new Game({ sudoku });
 }
 
 export function createGameFromJSON(json) {
-  const history = json.history.map(createSudokuFromJSON);
-  const game = new Game(history[0]);
-  game.history = history;
-  game.currentIndex = json.currentIndex;
-  game.current = history[game.currentIndex].clone();
-  return game;
+  return Game.fromJSON(json);
 }
+
+Sudoku.fromJSON = function (json) {
+  return new Sudoku(json.grid);
+};
+
+Game.fromJSON = function (json) {
+  const sudoku = Sudoku.fromJSON(json.sudoku);
+  const game = new Game({ sudoku });
+  game.history = json.history.map(h => Sudoku.fromJSON(h));
+  game.currentStep = json.currentStep;
+  game.sudoku = game.history[game.currentStep].clone();
+  return game;
+};
