31f2255569344e6abb7cb361ace8b05bfe79a78a
PDybala123 <18526620672@163.com>
2026-04-12 16:48:05 +0800

    Update index.svelte

--- src/components/Controls/index.svelte
+++ src/components/Controls/index.svelte
@@ -1,11 +1,22 @@
 <script>
 	import ActionBar from './ActionBar/index.svelte';
 	import Keyboard from './Keyboard.svelte';
+
+	import { gameStore } from '../../stores/gameStore';
 </script>
 
 <div class="px-4 pb-5 flex justify-center">
 	<div class="w-full max-w-xl">
 		<ActionBar />
+
+		<div class="flex gap-2 mt-2">
+			<button on:click={gameStore.undo} disabled={!$gameStore.canUndo}>
+				Undo
+			</button>
+			<button on:click={gameStore.redo} disabled={!$gameStore.canRedo}>
+				Redo
+			</button>
+		</div>
 
 		<Keyboard />
 	</div>
